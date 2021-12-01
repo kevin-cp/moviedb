2fa11146b981276ae4791d1692833002a2618704
Kevin-cp <kevinpeixoto182@gmail.com>
2021-12-01 23:08:58 +0100

    controller

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,17 +2,72 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * Contrôleur en mode "sandbox"
+ */
 class TestController extends AbstractController
 {
-    #[Route('/test', name: 'test')]
-    public function index(): Response
+    /** Test de création d'entité */
+    #[Route('/test/create', name: 'test_create')]
+    public function create(ManagerRegistry $doctrine): Response
     {
-        return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
-        ]);
+       // On créé une entité en PHP
+       $movie = new Movie();
+
+       // On renseigne l'entité
+       $movie->setTitle("Blade Runner");
+
+       // 1. On demande au Manager de se préparer à "persister" l'entité
+       $entityManager = $doctrine->getManager();
+       $entityManager->persist($movie);
+
+       // 2. 2xécute les requêtes SQL nécessaires (ici, INSERT INTO)
+       $entityManager->flush();
+
+       return new Response('Film ajouté :'.$movie->getId(). '</body>');
+       // PS : le </body> pour afficher la toolbar
+    }
+
+    /**
+     * @Route("/test/browse", name="test_browse")
+     */
+    public function browse(ManagerRegistry $doctrine)            
+    {
+        // Pour accéder aux données de la table movie
+        // on passe par le Reposirory de l'entité Movie
+        // PS : Movie::class => 'App\Entity\Movie'
+        $movieRepository = $doctrine->getRepository(Movie::class);
+
+        // On utilise les méthodes d'accès fournies par ce Repository
+        $movies = $movieRepository->findAll();
+
+        dump($movies);
+
+        return new Response('liste des films </body>');
+    }
+
+     /**
+     * @Route("/test/read/{id<\d+>}", name="read")
+     */
+    public function read(ManagerRegistry $doctrine, $id)            
+    {
+        // Pour accéder aux données de la table movie
+        // on passe par le Reposirory de l'entité Movie
+        // PS : Movie::class => 'App\Entity\Movie'
+        $movieRepository = $doctrine->getRepository(Movie::class);
+
+        // On utilise les méthodes d'accès fournies par ce Repository
+        $movie = $movieRepository->find($id);
+
+        dd($movie);
+
+        return new Response('film n'.$id.'</body>');
     }
 }
--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 // On va appliquer la logique de mapping via l'annotation @ORM
 // qui correspond à un dossier "Mapping" de Doctrine
+
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -32,6 +34,16 @@ class Movie
   private $title;
 
   /**
+   * @ORM\Column(type="datetime")
+   */
+  private $createdAt;
+
+  /**
+   * @ORM\Column(type="datetime", nullable=true)
+   */
+  private $updatedAt;
+
+  /**
    * Get clé primaire
    */ 
   public function getId()
@@ -52,9 +64,49 @@ class Movie
    *
    * @return  self
    */ 
-  public function setTitle($title)
+  public function setTitle(string $title)
   {
     $this->title = $title;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of createdAt
+   */ 
+  public function getCreatedAt()
+  {
+    return $this->createdAt;
+  }
+
+  /**
+   * Set the value of createdAt
+   *
+   * @return  self
+   */ 
+  public function setCreatedAt(DateTime $createdAt)
+  {
+    $this->createdAt = $createdAt;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of updatedAt
+   */ 
+  public function getUpdatedAt()
+  {
+    return $this->updatedAt;
+  }
+
+  /**
+   * Set the value of updatedAt
+   *
+   * @return  self
+   */ 
+  public function setUpdatedAt(DateTime $updatedAt)
+  {
+    $this->updatedAt = $updatedAt;
 
     return $this;
   }
